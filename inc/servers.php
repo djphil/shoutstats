78291da4e6fc6019142d8b48752c4b6ad1c15b83
djphil <djphil@users.noreply.github.com>
2020-06-20 15:15:04 +0200

    More indentation
    More indentation

--- inc/servers.php
+++ inc/servers.php
@@ -1,34 +1,34 @@
 <?php
 $servers = [
-	"Radio 1" => [
-		"ip" => "radio.deepmix.ru",
-		"port" => 8320,
-		"sid" => 1,
-		"type" => "mp3"
-	],
-	"Radio 2" => [
-		"ip" => "37.59.254.24",
-		"port" => 8024,
-		"sid" => 1,
-		"type" => "mp3"
-	],
-	"Radio 3" => [
-		"ip" => "37.187.0.6",
-		"port" => 9000,
-		"sid" => 1,
-		"type" => "mp3"
-	],
-	"Radio 4" => [
-		"ip" => "live.nsbradio.co.uk",
-		"port" => 8904,
-		"sid" => 1,
-		"type" => "mp3"
-	],
-	"Radio 5" => [
-		"ip" => "212.71.250.12",
-		"port" => 8000,
-		"sid" => 1,
-		"type" => "mp3"
-	],
+    "Radio 1" => [
+        "ip" => "radio.deepmix.ru",
+        "port" => 8320,
+        "sid" => 1,
+        "type" => "mp3"
+    ],
+    "Radio 2" => [
+        "ip" => "37.59.254.24",
+        "port" => 8024,
+        "sid" => 1,
+        "type" => "mp3"
+    ],
+    "Radio 3" => [
+        "ip" => "37.187.0.6",
+        "port" => 9000,
+        "sid" => 1,
+        "type" => "mp3"
+    ],
+    "Radio 4" => [
+        "ip" => "live.nsbradio.co.uk",
+        "port" => 8904,
+        "sid" => 1,
+        "type" => "mp3"
+    ],
+    "Radio 5" => [
+        "ip" => "212.71.250.12",
+        "port" => 8000,
+        "sid" => 1,
+        "type" => "mp3"
+    ],
 ];
 ?>
